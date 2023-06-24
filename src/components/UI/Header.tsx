c08edd233ca9314cd9eb3e1fca473dc84068cc8c
HondDK <Honddk99@gmail.com>
2023-06-24 16:45:32 +0600

    add PrivateComponents.tsx

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import Navbar from "./Navbar";
+import PrivateComponents from "../../helpers/PrivateComponents";
+import {Link} from "react-router-dom";
 
 const Header = () => {
     return (
         <header>
-            <img alt="logo"
-                 src={"https://sun9-33.userapi.com/impg/loTq0HF-9c9XuzGmfsorHtiqU643SsEiqZIkOw/9UwVmHbf5ow.jpg?size=195x48&quality=96&sign=c90e0a84ca6db05b622083b57cd9ea63&type=album"}/>
-            <p>Авторизация</p>
-            <Navbar ></Navbar>
+            <Link to={"/orders"}>
+                <img alt="logo"
+                     src={"https://sun9-33.userapi.com/impg/loTq0HF-9c9XuzGmfsorHtiqU643SsEiqZIkOw/9UwVmHbf5ow.jpg?size=195x48&quality=96&sign=c90e0a84ca6db05b622083b57cd9ea63&type=album"}/>
+            </Link>
+            <PrivateComponents>
+                <Navbar></Navbar>
+            </PrivateComponents>
+
         </header>
     )
 }
---
+++ src/helpers/PrivateComponents.tsx
@@ -0,0 +1,22 @@
+
+import { Link } from "react-router-dom";
+import {useAppSelector} from "../hooks/useRedux";
+import {HTMLProps} from "react";
+const PrivateComponents = ({ children }: HTMLProps<HTMLElement>) => {
+
+    const { access_token } = useAppSelector((state) => state.loginPage);
+
+    if (access_token) {
+        return <>{children}</>;
+    } else {
+        return (
+            <>
+                <Link to="/" relative="path" >
+                    <p>Авторизация</p>
+                </Link>
+            </>
+        );
+    }
+};
+
+export default PrivateComponents;
